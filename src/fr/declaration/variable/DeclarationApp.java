c51090b334cae3e0d677005f6c7a0e96aca2a827
brahimi sonia <sonia>
2022-06-07 22:52:58 +0200

    tp2

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,27 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+byte toto = 1;
+short x = -3;
+x++;
+x++;
+
+
+int a = 2;
+long c = 41585485L;
+float e = 1.3F ;
+double f = 2.3D;
+char g = 'a';
+char v = '\u20ac';
+boolean s = true; 
+String r = "test"; 
+String randomString1 = "Voici le résultat d’un calcul :" ;
+String randomString2 = "1+5=6";
+System.out.println(randomString1);
+System.out.println(randomString2);
+	}
+
+}
